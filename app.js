acbdbe86fd4d1a770d0bb92cf4f515689a63738a
Onakoya Kayode <onakoyakayode@gmail.com>
2021-12-24 15:02:26 +0100

    add Js file

---
+++ app.js
@@ -0,0 +1,15 @@
+let button = document.getElementById("button").innerHTML = "My Message";
+
+function clickMe() {
+    let fullName = prompt("Kindly, Tell me your name");
+    if (fullName.length < 6) {
+        document.getElementById("wish").innerHTML = fullName + "," + " Seasons greetings! Wishing you and your family all things bright and beautiful this Christmas!";
+    } else if (fullName.length <= 6) {
+        document.getElementById("wish").innerHTML = fullName + "," + " Wishing you the warmest of Christmas greetings, you will be in my thoughts during this special holiday.";
+    } else if (fullName.length >= 6) {
+        document.getElementById("wish").innerHTML = fullName + "," + " Seasons greetings! Wishing you and your family all things bright and beautiful this Christmas!";
+    }
+
+};
+
+
